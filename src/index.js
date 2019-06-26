810715a8780014752c8012f84493e6c2946ac23b
ellinx <ellin.xll@gmail.com>
2019-06-25 20:39:20 -0700

    Roll back package version to fix issues.

--- src/index.js
+++ src/index.js
@@ -7,31 +7,20 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { createFirestoreInstance, getFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase, } from 'react-redux-firebase'
-import firebase from './config/fbConfig'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase, } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer, compose(
-  applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-))
-
-const rrfConfig = {
-  userProfile: 'users',
-  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
-
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance
-}
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
       <App />
-    </ReactReduxFirebaseProvider>
   </Provider>, 
   document.getElementById('root')
 );
