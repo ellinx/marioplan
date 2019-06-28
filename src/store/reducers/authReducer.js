6c4e76de038da3c10195e1fafca91c1f693d8973
ellinx <ellin.xll@gmail.com>
2019-06-27 23:13:53 -0700

    Add login.

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,26 @@
-const initState = {}
+const initState = {
+  authError: null,
+}
 
 const authReducer = (state = initState, action) => {
+  switch(action.type) {
+    case 'LOGIN_ERROR':
+      console.log('login failed')
+      state = {
+        ...state,
+        authError: 'Login Failed'
+      }
+      break;
+    case 'LOGIN_SUCCESS':
+      console.log('login success')
+      state = {
+        ...state,
+        authError: null,
+      }
+      break
+    default:
+      break;
+  }
   return state
 }
 
