edcef5471198c4b266e9184c51aca971b8f5c61f
ellinx <ellin.xll@gmail.com>
2019-09-15 23:24:07 -0700

    Change adding new project with information from profile.

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,11 +2,13 @@ export const createProject = (project) => {
   return (dispatch, getState, {getFirestore}) => {
     // make asynchrous call to database
     const firestore = getFirestore()
+    const profile = getState().firebase.profile
+    const authorId = getState().firebase.auth.uid
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'LL',
-      authorLastName: 'XX',
-      authorId: 1234,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date()
     }).then(()=>{
       dispatch({type: 'CREATE_PROJECT', project})
